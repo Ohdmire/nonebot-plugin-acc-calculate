1cad972dca7a45683644a42f671793679760f81e
10-24 <1750011571@qq.com>
2022-11-27 15:23:33 +0800

    Update __init__.py

--- nonebot_plugin_acc_calculate/__init__.py
+++ nonebot_plugin_acc_calculate/__init__.py
@@ -5,7 +5,7 @@ from .data_source import calculate_acc
 
 malody = on_command('acc')
 
-@malody.got("t", prompt='请输入要查询的段位如:ex2 regular1')
+@malody.got("t", prompt='请输入要查询的段位(regular* regular*v2 ex* ex*v2 reform*)')
 @malody.got('accs', prompt='请按顺序输入acc如：99.26-98.63-97.32-96.11')
 async def _(event: MessageEvent,t=ArgStr("t"),accs=ArgStr("accs")):
     accs=accs.strip().split("-")
