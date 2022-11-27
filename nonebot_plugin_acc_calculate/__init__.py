255bb7af934fae1913563aa339fdf3a14ddb735d
10-24 <1750011571@qq.com>
2022-11-27 16:06:25 +0800

    v0.32

--- nonebot_plugin_acc_calculate/__init__.py
+++ nonebot_plugin_acc_calculate/__init__.py
@@ -3,9 +3,9 @@ from nonebot.params import ArgStr
 from nonebot import on_command
 from .data_source import calculate_acc
 
-malody = on_command('acc')
+malody = on_command('/acc')
 
-@malody.got("t", prompt='请输入要查询的段位(regular* regular*v2 ex* ex*v2 reform*)')
+@malody.got("t", prompt='请输入要查询的段位(regular*|regular*v2|ex*|ex*v2|reform*)[*换成对应的数字或字母]')
 @malody.got('accs', prompt='请按顺序输入acc如：99.26-98.63-97.32-96.11')
 async def _(event: MessageEvent,t=ArgStr("t"),accs=ArgStr("accs")):
     accs=accs.strip().split("-")
--- setup.py
+++ setup.py
@@ -1,34 +1,25 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+import setuptools
 
 with open("README.md", "r",encoding="UTF-8") as f:
     long_description = f.read()
 
-
-packages = \
-['nonebot_plugin_acc_calculate']
-
-package_data = \
-{'': ['*']}
-
-install_requires = \
-['nonebot-adapter-onebot>=2.0.0-beta.1,<3.0.0', 'nonebot2>=2.0.0-beta.4,<3.0.0']
-
-setup_kwargs = {
-    'name': 'nonebot-plugin-acc-calculate',
-    'version': '0.3.0',
-    'description': 'Nonebot2 段位单曲acc计算',
-    'long_description':long_description,
-    'author': 'ohdmire',
-    'author_email': '1750011571@qq.com',
-    'maintainer': None,
-    'maintainer_email': None,
-    'url': 'https://github.com/ohdmire/nonebot-plugin-acc-calculate',
-    'packages': packages,
-    'package_data': package_data,
-    'install_requires': install_requires,
-    'python_requires': '>=3.7.3,<4.0.0',
-}
-
-
-setup(**setup_kwargs)
+setuptools.setup(
+    name="nonebot-plugin-acc-calculate",
+    version="0.3.2",
+    author="ohdmire",
+    author_email="1750011571@qq.com",
+    description="Nonebot2 段位单曲acc计算",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ohdmire/nonebot-plugin-acc-calculate",
+    project_urls={
+        "Bug Tracker": "https://github.com/ohdmire/nonebot-plugin-acc-calculate/issues",
+    },
+    packages=["nonebot_plugin_acc_calculate"],
+    python_requires=">=3.8,<4.0",
+    install_requires=[
+        "nonebot2>=2.0.0a16",
+        "nonebot-adapter-onebot>=2.0.0b1",
+    ],
+)
